1c307f83f6c0925f7fb85c9e93697c8c6ed6e8c7
SantaGosu <taz.c.munoz@gmail.com>
2022-10-01 23:37:54 +0000

    left off after setup route to get all users

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(15), unique=False, nullable=False)
+    last_name = db.Column(db.String(15), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +16,24 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    population = db.Column(db.String(50), nullable=False)
+    terrain = db.Column(db.String(15), nullable=False)
+    climate = db.Column(db.String(50), nullable=False)
+
+    def serialize(self):
+        return {
+        "id": self.id,
+        "name": self.name,
+        "population": self.population,
+        "terrain": self.terrain,
+        "climate": self.climate
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Planets
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,25 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/api/users', methods=['POST'])
+def create_user(request):
+    request_body = request.get_json(),
+    new_user = User(
+        first_name=request_body['first_name'],
+        last_name=request_body['last_name'],
+        email=request_body['email'],
+        password=request_body['password'],
+        is_active=True
+        )
+    db.session.add(new_user)
+    db.session.commit()
+    return f"A new user has been added"
+
+@api.route('/api/users', methods=['GET'])
+def get_users(request):
+    allUsers = User.query.all()
+    userList = list(map(lambda x: x.serialize, allUsers))
+    
+    return jsonify(userList), 200
