dc9a94f50b50c8d322f7bf22d45bf4fbbe76fd84
SantaGosu <taz.c.munoz@gmail.com>
2022-10-02 22:00:43 +0000

    made routes for people and added their information to the database

--- src/api/routes.py
+++ src/api/routes.py
@@ -66,3 +66,28 @@ def get_planets(request):
 @api.route('/api/planet/delete', methods=['DELETE'])
 def delete_planet(request):
     db.session.delete(self)
+
+@api.route('/api/people', methods=['POST')
+def create_person(request):
+    request_body = request.get_json(),
+    new_person = Person(
+        name=request_body['name'],
+        gender=request_body['gender'],
+        eye_color=request_body['eye_color'],
+        skin_color=request_body['skin_color'],
+        height=request_body['height']
+    )
+    db.session.add(new_person)
+    db.session.commit()
+    return f"A new person has been added"
+
+@api.route('/api/people', methods=['GET'])
+def get_people(request):
+    allPeople = Person.query.all()
+    personList = list(map(lambda x: x.serialize, allPeople))
+    return jsonify(personList), 200
+
+@api.route('/api/people/delete', methods=['DELETE'])
+def delete_person(request):
+    db.session.delete(self)
+    return f"A new person has been deleted"
